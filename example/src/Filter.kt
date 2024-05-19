b45e0308b32a2ca7c9a2e5d4af0f2c6b76ed8322
Yurn <ChenDSakura@163.com>
2024-05-19 21:07:32 +0800

    删除 example

--- example/src/Filter.kt
+++
@@ -1,5 +0,0 @@
-package github.nyayurn.qbot
-
-import github.nyayurn.yutori_next.MessageEvent
-
-val qqHelperFilter = { event: MessageEvent -> event.platform == "chronocat" && event.user.id == "2854196310" }
